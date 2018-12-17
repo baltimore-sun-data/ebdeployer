8ef74478867cad7edaecadf8cbe6c446dbfa02fe
Carl Johnson <me@carlmjohnson.net>
2018-12-17 12:51:42 -0500

    EB: Fix date formatting bug

--- elasticbeanstalk.go
+++ elasticbeanstalk.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"strings"
 	"time"
 )
 
 func createEBEnv(repo, cfg string, date time.Time) error {
-	env := repo + "-" + date.Format("jan2-1504")
+	env := repo + "-" + date.Format("Jan2-1504")
+	env = strings.ToLower(env)
 	err := subprocess("", "eb", "create", env, "--cfg", cfg)
 	if err != nil {
 		return err
